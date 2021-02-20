bf3fb5fc5c607ad5723e4633d03e597ca7cf040e
jianghaiqiang <jianghaiqiang@360.cn>
2021-02-20 12:17:14 +0800

    graceful

--- examples/graceful/main.go
+++ examples/graceful/main.go
@@ -1,49 +1,24 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net"
 	"net/http"
 
 	"github.com/grpc-boot/boot/grace"
 )
 
-type HttpServer struct {
-	grace.Server
-
-	server *http.Server
-}
-
-func (hs *HttpServer) Serve(l net.Listener) error {
-	return hs.server.Serve(l)
-}
-
-func (hs *HttpServer) Shutdown(ctx context.Context) error {
-	fmt.Println("shutdown")
-	return hs.server.Shutdown(ctx)
-}
-
 func main() {
-	fmt.Println("begin")
-
-	httpServer := &HttpServer{
-		server: &http.Server{},
-	}
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte("ok"))
+	})
 
 	server, err := grace.NewTcpServer(&grace.Option{
 		Addr:   ":8090",
-		Server: httpServer,
+		Server: &http.Server{},
 	})
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(request.RequestURI)
-		_, _ = writer.Write([]byte("ok"))
-	})
-
-	server.Serve(nil)
+	server.Serve()
 }
--- grace/grace.go
+++ grace/grace.go
@@ -3,17 +3,17 @@ package grace
 import (
 	"context"
 	"errors"
-	"fmt"
+	"flag"
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 	"time"
 )
 
 const (
-	gracefulKey        = "GRACEFUL"
 	gracefulListenerFd = 3
 )
 
@@ -33,145 +33,79 @@ type Server interface {
 
 type TcpServer struct {
 	signalChan chan os.Signal
-	clearChan  chan bool
 	listener   net.Listener
 	server     Server
 }
 
 func NewTcpServer(option *Option) (server *TcpServer, err error) {
+	grace := flag.Bool("graceful", false, "listen on fd open 3 (internal use only)")
+	flag.Parse()
+
 	var listener net.Listener
-	listener, err = net.Listen("tcp", option.Addr)
-	if err != nil {
-		return nil, err
+	if *grace {
+		file := os.NewFile(gracefulListenerFd, "")
+		listener, err = net.FileListener(file)
+		if err != nil {
+			return nil, ErrGetListenerFdFailed
+		}
+	} else {
+		listener, err = net.Listen("tcp", option.Addr)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	server = &TcpServer{
 		signalChan: make(chan os.Signal, 1),
-		clearChan:  make(chan bool, 1),
 		listener:   listener,
 		server:     option.Server,
 	}
 	return
 }
 
-func (ts *TcpServer) Listener() net.Listener {
-	return ts.listener
-}
-
-func (ts *TcpServer) startServe(l net.Listener) {
-	ts.listener = l
+func (ts *TcpServer) Serve() {
 	go func() {
 		if err := ts.server.Serve(ts.listener); err != nil {
 			log.Println("error:", err.Error())
 		}
 	}()
+
+	for {
+		signal.Notify(ts.signalChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGUSR2)
+		sig := <-ts.signalChan
+		switch sig {
+		case syscall.SIGINT, syscall.SIGTERM:
+			_ = ts.shutdown()
+			signal.Stop(ts.signalChan)
+			return
+		case syscall.SIGHUP, syscall.SIGUSR2:
+			if err := ts.reload(); err != nil {
+				log.Println("error:", err.Error())
+				continue
+			}
+
+			_ = ts.shutdown()
+			return
+		}
+	}
+}
+
+func (ts *TcpServer) reload() (err error) {
+	listenerFd, err := ts.listener.(*net.TCPListener).File()
+	if err != nil {
+		return ErrGetListenerFdFailed
+	}
+
+	args := []string{"-graceful"}
+	cmd := exec.Command(os.Args[0], args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.ExtraFiles = []*os.File{listenerFd}
+	return cmd.Start()
 }
 
 func (ts *TcpServer) shutdown() (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 	return ts.server.Shutdown(ctx)
-}
-
-func (ts *TcpServer) Serve(envList []string) {
-	ts.startServe(ts.listener)
-	log.Println("start")
-
-	go func() {
-		for {
-			signal.Notify(ts.signalChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
-			sig := <-ts.signalChan
-			switch sig {
-			case syscall.SIGINT, syscall.SIGTERM:
-				fmt.Println("INT")
-				ts.clearChan <- true
-			case syscall.SIGHUP:
-				fmt.Println("HUP")
-				listener, _, err := Fork(envList)
-				if err != nil {
-					log.Println("error:", err.Error())
-					continue
-				}
-
-				ts.startServe(listener)
-				log.Println("grace")
-
-				err = StopOldProcess()
-				if err != nil {
-					log.Println("error:", err.Error())
-				}
-			}
-		}
-	}()
-	fmt.Println("clear")
-	<-ts.clearChan
-	if err := ts.shutdown(); err != nil {
-		log.Println("error:", err.Error())
-	}
-}
-
-func (ts *TcpServer) Wait() {
-	cleanupDone := make(chan bool)
-	go func() {
-		for {
-			signal.Notify(ts.signalChan, syscall.SIGINT, syscall.SIGTERM)
-			sig := <-ts.signalChan
-			switch sig {
-			case syscall.SIGINT, syscall.SIGTERM:
-				cleanupDone <- true
-			}
-		}
-	}()
-	<-cleanupDone
-	if err := ts.shutdown(); err != nil {
-		log.Println("error:", err.Error())
-	}
-}
-
-func Fork(envList []string) (listener net.Listener, process *os.Process, err error) {
-	file := os.NewFile(gracefulListenerFd, "")
-	listener, err = net.FileListener(file)
-	if err != nil {
-		return nil, nil, ErrGetListenerFdFailed
-	}
-
-	var listenerFd *os.File
-	listenerFd, err = listener.(*net.TCPListener).File()
-	if err != nil {
-		return nil, nil, ErrGetListenerFdFailed
-	}
-
-	procAtt := &os.ProcAttr{
-		Files: []*os.File{
-			os.Stdin,
-			os.Stdout,
-			os.Stderr,
-			listenerFd,
-		},
-	}
-
-	if len(envList) > 0 {
-		procAtt.Env = envList
-	}
-
-	argv := make([]string, 0)
-	for _, arg := range os.Args {
-		if arg == gracefulKey {
-			break
-		}
-		argv = append(argv, arg)
-	}
-	argv = append(argv, gracefulKey)
-
-	process, err = os.StartProcess(os.Args[0], argv, procAtt)
-	return
-}
-
-func StopOldProcess() (err error) {
-	process, err := os.FindProcess(os.Getppid())
-	if err != nil {
-		return err
-	}
-
-	return process.Signal(syscall.SIGTERM)
 }
