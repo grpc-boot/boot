e592e98d86755b0988abc44eb2492c63080bdb7d
jianghaiqiang <jhq0113@163.com>
2021-10-18 14:31:35 +0800

    fixed hash

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -1,10 +1,16 @@
 package hash
 
 import (
+	"crypto"
+	"crypto/md5"
+	"crypto/sha1"
+	"fmt"
 	"hash/crc32"
 	"strconv"
 	"testing"
+	"time"
 
+	"github.com/grpc-boot/boot"
 	"github.com/grpc-boot/boot/atomic"
 )
 
@@ -28,7 +34,7 @@ type Group struct {
 }
 
 type Db struct {
-	CanHash
+	boot.CanHash
 	id []byte
 }
 
@@ -39,7 +45,7 @@ func (d *Db) HashCode() (hashValue uint32) {
 // go test -bench=. -benchmem -v
 // BenchmarkHashRing_GetIndex-4    24148118                65.9 ns/op            16 B/op          1 allocs/op
 func BenchmarkHashRing_GetIndex(b *testing.B) {
-	serverList := make([]CanHash, 0, len(hostList))
+	serverList := make([]boot.CanHash, 0, len(hostList))
 
 	for _, server := range hostList {
 		serverList = append(serverList, &Db{
@@ -57,6 +63,52 @@ func BenchmarkHashRing_GetIndex(b *testing.B) {
 			if err != nil {
 				b.Fatal(err.Error())
 			}
+		}
+	})
+}
+
+func TestMd5(t *testing.T) {
+	data := []byte(time.Now().String())
+	hashMd5 := Md5(data)
+	t.Log(hashMd5)
+
+	oMd5 := fmt.Sprintf("%x", md5.Sum(data))
+	t.Log(oMd5)
+	if hashMd5 != oMd5 {
+		t.Fatal("want true, got false")
+	}
+	t.Log(string(data))
+}
+
+func TestSha1(t *testing.T) {
+	data := []byte(time.Now().String())
+	hashSha1 := Sha1(data)
+	t.Log(hashSha1)
+
+	oSha1 := fmt.Sprintf("%x", sha1.Sum(data))
+	t.Log(oSha1)
+	if hashSha1 != oSha1 {
+		t.Fatal("want true, got false")
+	}
+	t.Log(string(data))
+}
+
+func TestHMac(t *testing.T) {
+	data := time.Now().String()
+	key := strconv.FormatInt(time.Now().UnixNano(), 10)
+	t.Log(data, key)
+
+	hSha1 := HMac([]byte(key), []byte(data), crypto.SHA1)
+	t.Log(hSha1)
+}
+
+func BenchmarkHash(b *testing.B) {
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			data := strconv.FormatInt(time.Now().UnixNano(), 10)
+			//Md5([]byte(data))
+			Hash([]byte(data), crypto.MD5)
 		}
 	})
 }
